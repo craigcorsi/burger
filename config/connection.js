6f7011baf0736551a6732a4ffa6c7d655c1a5da9
Craig <corsi007@umn.edu>
2018-07-24 20:05:51 -0500

    connect to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,25 @@
 // mysql node package allows us to establish a connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+  });
+}
+
+
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
